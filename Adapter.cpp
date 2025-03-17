8543dcac1c11fdf9c411973f430cce88841b32f2
Irene Matlyak <iryne.matlyak@gmail.com>
2025-03-17 21:02:02 +0100

    Add files via upload

---
+++ Adapter.cpp
@@ -0,0 +1,114 @@
+#include <memory>               // For std::shared_ptr
+#include <boost/shared_ptr.hpp> // For boost::shared_ptr
+#include <boost/make_shared.hpp>
+#include <iostream>
+#include <type_traits>
+
+
+
+
+// Product
+class Product
+{
+    const char* _name = "RC toy" ;
+public: 
+    // clang tidy : [modernize-use-nodiscard]   
+    [[nodiscard]] constexpr const char* get_name() const noexcept {return _name;}
+    constexpr void set_name(const char* name){_name = name;}
+
+    Product() = default;
+
+    constexpr Product(const Product& other) = default;
+    Product& operator=(const Product& other) = default;
+    constexpr Product( Product&& other) = default;
+    Product& operator=( Product&& other) = default;
+    virtual ~Product() = default;
+
+};
+
+// Concreate product
+class Car : public Product
+{
+public:    
+    Car(){set_name("RC toy car");}
+};
+
+class Helicopte : public Product
+{ 
+public:
+    Helicopte(){set_name("RC toy helicopter");}
+};
+
+class Boat : public Product
+{
+public:    
+    Boat(){set_name("RC toy boat");}
+};
+
+enum class Catalog : uint8_t {CARS, HELICOPTER, BOAT};
+
+class Object_Factory_lmplementation //: public ObjectFactory
+{
+    public:
+    Object_Factory_lmplementation() = default;
+
+    [[nodiscard]] std::shared_ptr<Product> create_object(const Catalog item) const
+    {
+        std::shared_ptr<Product> result = nullptr;
+        switch(item)
+        {
+            case Catalog::CARS:
+                result = std::make_shared<Car>(); 
+                break;
+            case Catalog::HELICOPTER:
+                result =  std::make_shared<Helicopte>();
+                break;
+            case Catalog::BOAT:
+                result = std::make_shared<Boat>();
+                break;
+            default:
+                std::cout << "This product is not supported yet";  
+        }
+        return result;
+    }
+
+};
+
+
+template<typename Underlying_type_t>
+class Smart_pointer_factory_adapter
+{
+public:
+
+    [[nodiscard]] boost::shared_ptr<Underlying_type_t> make_shared(std::shared_ptr<Underlying_type_t>&& std_pointer)
+    {
+        return boost::make_shared<Underlying_type_t>(std::move(*std_pointer));
+    };
+
+    // To unclude unique pointer functionality
+    // #include <boost/move/unique_ptr.hpp>
+    // #include <boost/make_unique.hpp>
+    /*[[nodiscard]] boost::unique_ptr<Underlying_type_t> make_unique(std::unique_ptr<Underlying_type_t>&& std_pointer)
+    {
+        return boost::make_unique<Underlying_type_t>(std::move(*std_pointer));
+    };*/
+
+};
+
+
+
+int main()
+{
+    Object_Factory_lmplementation* factory_object = new Object_Factory_lmplementation();
+    
+    // but i want a boost pointer to handle my object :( or std im so unsure !!
+    Smart_pointer_factory_adapter<Product> adapter;
+
+    boost::shared_ptr<Product> what_i_want  = adapter.make_shared(factory_object->create_object(Catalog::HELICOPTER));
+    std::cout << what_i_want->get_name() << "\n";
+
+    return 0;
+}
+
+//  https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2003/n1450.html
+//  A Proposal to Add General Purpose Smart Pointers to the Library Technical Report
---
+++ Decorator.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <ctime>
+#include <windows.h>
+
+// Base Logger interface
+template<typename T>
+class Logger {
+public:
+    virtual void log(const T& message) const = 0;
+    virtual ~Logger() = default;
+};
+
+// Concrete Logger implementation
+template<typename T>
+class BasicLogger : public Logger<T> {
+public:
+    void log(const T& message) const override {
+        std::cout << "Log: " << message << std::endl;
+    }
+};
+
+// Decorator for adding timestamps
+template<typename T>
+class TimestampLogger : public Logger<T> {
+private:
+    std::shared_ptr<Logger<T>> logger_;
+public:
+    explicit TimestampLogger(std::shared_ptr<Logger<T>> logger) : logger_(std::move(logger)){}
+
+    void log(const T& message) const override {
+        const std::time_t now = std::time(nullptr);
+        char timeString[30]{};
+        std::strftime(timeString, sizeof(timeString), "%A %c", std::localtime(&now)); // https://en.cppreference.com/w/cpp/chrono/c/strftime
+        std::cout << "[Timestamp: " <<  timeString << "] ";
+        logger_->log(message);
+    }
+};
+
+// Decorator for adding user authentication info
+template<typename T>
+class AuthLogger : public Logger<T> {
+private:
+    std::shared_ptr<Logger<T>> logger_;
+public:
+    explicit AuthLogger(std::shared_ptr<Logger<T>> logger) : logger_(std::move(logger)) {}
+
+    void log(const T& message) const override {
+        unsigned __LONG32 buffer_length = 20UL;
+        char buffer_name[buffer_length]{};
+
+        if ( GetUserNameA(buffer_name, &buffer_length) )
+        {
+            std::cout << "[" << buffer_name <<"] ";
+        }
+        else{
+            std::cout << "[WARNING : failed to retrieve user] ";
+        }
+        logger_->log(message);
+    }
+};
+
+// Decorator for adding request context
+template<typename T>
+class RequestContextLogger : public Logger<T> {
+private:
+    std::shared_ptr<Logger<T>> logger_;
+public:
+    explicit RequestContextLogger(std::shared_ptr<Logger<T>> logger) : logger_(std::move(logger)) {}
+
+    void log(const T& message) const override {
+        std::cout << "[Request ID: 12345] ";
+        logger_->log(message);
+    }
+};
+
+int main() {
+    // Create the basic logger
+    auto basicLogger = std::make_shared<BasicLogger<std::string>>();
+
+    // Apply decorators to add functionality
+    auto timestampedLogger = std::make_shared<TimestampLogger<std::string>>(basicLogger);
+    auto authLogger = std::make_shared<AuthLogger<std::string>>(timestampedLogger);
+    auto requestContextLogger = std::make_shared<RequestContextLogger<std::string>>(authLogger);
+    
+
+    // Log a message with all decorators applied
+    requestContextLogger->log("API request received");
+
+    return 0;
+}
