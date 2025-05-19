6768d18c4fea4943b2a804188f4653ba3915e8f4
Irene Matlyak <iryne.matlyak@gmail.com>
2025-05-19 23:30:27 +0200

    Update Prototype.cpp
    clean code  improvements

--- Prototype.cpp
+++ Prototype.cpp
@@ -1,15 +1,16 @@
+#include <chrono>
 #include <cstdint>
 #include <iostream>
-#include <thread>
-#include <chrono>
 #include <memory>
-#include <utility>
+#include <thread>
 
 enum class CARS : uint8_t {LAMBORGINI, CHEVROLET};
 
 // This is trivial 
 class Car_data
 {
+    // TODO(Iryna, till 19.05): bug [bugprone-easily-swappable-parameters] Obviously put this in struct for safety.
+
     float _engine_volume{};
     int _top_speed{};
     float _acceleration{};
@@ -23,25 +24,36 @@ public:
 
     void show_top_speed() const
     {std::cout << "Top speed is: " << _top_speed << "\n";}
+    void show_engine_volume() const
+    {std::cout << "Top speed is: " << _engine_volume << "\n";}
+    void show_accelerationd() const
+    {std::cout << "Top speed is: " << _acceleration << "\n";}
+    void show_kerbWeight() const
+    {std::cout << "Top speed is: " << _kerbWeight << "\n";}
 
 
 };
 
+// TODO(Iryna, till 19.05) bug: Sonar - Classes should not contain both public and private data members. Set _data private. Make a seter for _data
 class Car
 {
     int _color{};
-public: 
-    std::shared_ptr<Car_data> _data = nullptr;
-
+// Justification: https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#c67-a-polymorphic-class-should-suppress-public-copymove
+protected:
     Car(const Car& other):_data(other._data){};
     Car& operator=(const Car& other) = default;
     Car( Car&& other) = default;
     Car& operator=( Car&& other) = default;
-    virtual ~Car() = default;
+// --- end of afected code block ---
+public: 
+    std::shared_ptr<Car_data> _data = nullptr;
 
+
+    virtual ~Car() = default;
     explicit Car(int color):_color(color){}
 
     virtual std::unique_ptr<Car> clone() = 0;
+    [[ nodiscard]] constexpr int getColor() const {return _color;}
 };
 
 
@@ -57,7 +69,7 @@ public:
         std::this_thread::sleep_for(std::chrono::milliseconds(3600));
     }; 
     Lamborgini_Diablo_SV() = delete;
-    ~Lamborgini_Diablo_SV()final = default;
+    ~Lamborgini_Diablo_SV() override = default;
 
     Lamborgini_Diablo_SV(const Lamborgini_Diablo_SV& other) = default;
     
@@ -80,7 +92,7 @@ public:
         std::this_thread::sleep_for(std::chrono::milliseconds(3600));
     };
     Chevrolet_Corvette() = delete;
-    ~Chevrolet_Corvette()final = default;
+    ~Chevrolet_Corvette() override = default;
     Chevrolet_Corvette(const Chevrolet_Corvette& other) = default;
 
     std::unique_ptr<Car> clone() override
@@ -119,10 +131,11 @@ int main()
     // Chosing from drop down menu a car for the race
     
     auto car_factory = std::make_unique<Car_factory>();
+    std::cout << "Prepare for race ! 3, 2, 1... \n";
     auto my_chevrolet =  car_factory->create_car(CARS::CHEVROLET);
 
     // Fast redrawing of a player model onto racing track
-    std::cout << "Prepare for race ! 3, 2, 1... \n";
+
     auto cpu_player1 = my_chevrolet->clone();
     auto cpu_player2 = my_chevrolet->clone();
 
